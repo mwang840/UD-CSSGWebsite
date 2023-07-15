3eef7194f1cc8b3dfdab11116fcd85b5e4129063
mwang840 <maxwang@udel.edu>
2023-07-14 22:31:02 -0600

    navbar set up initially

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Navbar } from "./components/NavBar";
 
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="about" />
+        </Routes>
+      </BrowserRouter>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
