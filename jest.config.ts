8986d5929abe55f1ddb9e4eba37d15090f562235
cthacker-udel <cthacker@udel.edu>
2023-08-05 08:38:42 -0400

    [csAbout] Converted to NextJs App

--- jest.config.ts
+++ jest.config.ts
@@ -1,19 +1,11 @@
+import nextJest from "next/jest";
 import type { Config } from "jest";
 
-const config: Config = {
-  moduleDirectories: ["src", "src/components", "node_modules"],
-  moduleNameMapper: {
-    "^@/components/pages/(.*)$": "<rootDir>/src/components/pages/$1",
-    "^@/components/assets/(.*)$": "<rootDir>/src/mocks/imgmock.ts",
-    "^.+\\.(css|less)$": "<rootDir>/src/mocks/cssmock.ts",
-    "^@/components/common/(.*)$": "<rootDir>/src/components/common/$1",
-  },
-  transform: {
-    "^.+\\.(ts|tsx|js|jsx)$": "ts-jest",
-  },
-  injectGlobals: true,
-  testEnvironment: "jsdom",
-  setupFilesAfterEnv: ["<rootDir>/setupfiles.ts"],
+const createJestConfig = nextJest({ dir: "./" });
+
+const customJestConfig: Config = {
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
+  testEnvironment: "jest-environment-jsdom",
 };
 
-export default config;
+export default createJestConfig(customJestConfig);
--- src/mocks/cssmock.ts
+++
@@ -1,2 +0,0 @@
-const cssMock = {};
-export default cssMock;
