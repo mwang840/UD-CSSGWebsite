a19f17dcc11847950d7998757830661c0f1a753a
cthacker-udel <cthacker@udel.edu>
2023-08-05 08:54:36 -0400

    [csAbout] Rendered navbar in app

--- src/components/common/Navbar/Navbar.tsx
+++ src/components/common/Navbar/Navbar.tsx
@@ -1,75 +1,15 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
 import "./Navbar.css";
+import Link from "next/link";
 
 export const Navbar = () => {
   return (
     <div className="navbar-container">
-      <NavLink
-        to="/home"
-        style={{
-          textDecoration: "none",
-          color: "black",
-          fontSize: "20px",
-          fontFamily: "Georgia",
-          borderRadius: "10px",
-          backgroundColor: "white",
-        }}
-      >
-        Home
-      </NavLink>
-      <NavLink
-        to="/about"
-        style={{
-          textDecoration: "none",
-          color: "black",
-          fontSize: "20px",
-          fontFamily: "Georgia",
-          borderRadius: "10px",
-          backgroundColor: "white",
-        }}
-      >
-        About
-      </NavLink>
-      <NavLink
-        to="/events"
-        style={{
-          textDecoration: "none",
-          color: "black",
-          fontSize: "20px",
-          fontFamily: "Georgia",
-          borderRadius: "10px",
-          backgroundColor: "white",
-        }}
-      >
-        Events
-      </NavLink>
-      <NavLink
-        to="/eboard"
-        style={{
-          textDecoration: "none",
-          color: "black",
-          fontSize: "20px",
-          fontFamily: "Georgia",
-          borderRadius: "10px",
-          backgroundColor: "white",
-        }}
-      >
-        Eboard
-      </NavLink>
-      <NavLink
-        to="/FAQ"
-        style={{
-          textDecoration: "none",
-          color: "black",
-          fontSize: "20px",
-          fontFamily: "Georgia",
-          borderRadius: "10px",
-          backgroundColor: "white",
-        }}
-      >
-        FAQ
-      </NavLink>
+      <Link href="/home">Home</Link>
+      <Link href="/about">About</Link>
+      <Link href="/events">Events</Link>
+      <Link href="/eboard">Eboard</Link>
+      <Link href="/FAQ">FAQ</Link>
     </div>
   );
 };
