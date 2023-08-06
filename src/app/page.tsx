5a78833eb0516a8fab85c344175665cd241c626a
cthacker-udel <cthacker@udel.edu>
2023-08-06 08:12:54 -0400

    [csAbout] Added structure of website

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,8 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 
-export default function Home() {
-  return (
-    <main>
-      <div>{"Hello"}</div>
-    </main>
-  );
-}
+const Home = (): JSX.Element => {
+  return <div>{"Hello"}</div>;
+};
+
+export default Home;
