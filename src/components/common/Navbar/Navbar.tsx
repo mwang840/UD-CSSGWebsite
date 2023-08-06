77a075e7c55512ed1cbc6635e8cb59a98f2a0f15
cthacker-udel <cthacker@udel.edu>
2023-08-06 08:00:51 -0400

    [csAbout] Improved navbar

--- src/components/common/Navbar/Navbar.tsx
+++ src/components/common/Navbar/Navbar.tsx
@@ -1,15 +1,56 @@
+"use client";
 import React from "react";
-import "./Navbar.css";
+import styles from "./Navbar.module.css";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+type NavbarProperties = {
+  currentLink: string;
+};
+
+enum NavbarPaths {
+  ABOUT = "/about",
+  EVENTS = "/events",
+  EBOARD = "/eboard",
+  FAQ = "/faq",
+  HOME = "/home",
+}
 
 export const Navbar = () => {
+  const path = usePathname();
+
   return (
-    <div className="navbar-container">
-      <Link href="/home">Home</Link>
-      <Link href="/about">About</Link>
-      <Link href="/events">Events</Link>
-      <Link href="/eboard">Eboard</Link>
-      <Link href="/FAQ">FAQ</Link>
+    <div className={styles.navbar_container}>
+      <Link
+        className={path === NavbarPaths.HOME ? styles.active_link : ""}
+        href="/home"
+      >
+        {"Home"}
+      </Link>
+      <Link
+        className={path === NavbarPaths.ABOUT ? styles.active_link : ""}
+        href="/about"
+      >
+        {"About"}
+      </Link>
+      <Link
+        className={path === NavbarPaths.EVENTS ? styles.active_link : ""}
+        href="/events"
+      >
+        {"Events"}
+      </Link>
+      <Link
+        className={path === NavbarPaths.EBOARD ? styles.active_link : ""}
+        href="/eboard"
+      >
+        {"Eboard"}
+      </Link>
+      <Link
+        className={path === NavbarPaths.FAQ ? styles.active_link : ""}
+        href="/FAQ"
+      >
+        {"FAQ"}
+      </Link>
     </div>
   );
 };
