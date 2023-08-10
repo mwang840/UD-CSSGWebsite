67fbc9afda5714982f5b48af85fa37a488310d2c
cthacker-udel <cthacker@udel.edu>
2023-08-10 02:34:08 -0400

    [landing_page] Added beginning of landing page to udcssg website

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-page-custom-font -- disabled, warning me for adding it to _document.js, when it's layout.tsx */
 /* eslint-disable new-cap -- disabled */
 import "./globals.css";
 
@@ -16,6 +17,28 @@ const inter = Inter({ subsets: ["latin"] });
  */
 const RootLayout = ({ children }: React.PropsWithChildren): JSX.Element => (
     <html lang="en">
+        <head>
+            <link
+                crossOrigin="anonymous"
+                href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css"
+                integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM"
+                rel="stylesheet"
+            />
+            <link
+                href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css"
+                rel="stylesheet"
+            />
+            <link href="https://fonts.googleapis.com" rel="preconnect" />
+            <link
+                crossOrigin=""
+                href="https://fonts.gstatic.com"
+                rel="preconnect"
+            />
+            <link
+                href="https://fonts.googleapis.com/css2?family=DynaPuff:wght@500&family=Golos+Text&family=Oswald:wght@300&family=Radio+Canada&family=Zeyada&display=swap"
+                rel="stylesheet"
+            />
+        </head>
         <body
             className={`${inter.className} ${styles.layout_body}`}
             id="landing_layout"
@@ -26,7 +49,14 @@ const RootLayout = ({ children }: React.PropsWithChildren): JSX.Element => (
 );
 
 export const metadata: Metadata = {
+    applicationName: "CS+SG Website",
+    authors: [
+        { name: "Maxwell Wang", url: "https://github.com/mwang840" },
+        { name: "Cameron Thacker", url: "https://github.com/cthacker-udel" },
+    ],
+    creator: "Cameron Thacker",
     description: "Created by Maxwell Wang + Cameron Thacker",
+    publisher: "Vercel",
     title: "CS+SG Website",
 };
 
--- src/modules/Landing/Landing.tsx
+++ src/modules/Landing/Landing.tsx
@@ -1,5 +1,6 @@
 "use client";
 import React from "react";
+import { Button } from "react-bootstrap";
 
 import _cssg from "@/assets/cs+sg.jpg";
 
@@ -16,6 +17,14 @@ const Landing = (): JSX.Element => {
         <div className={styles.landing_container}>
             <div className={styles.landing_header}>
                 {"Welcome to UD CS + Social Good!"}
+            </div>
+            <div className={styles.options_bar}>
+                <Button
+                    className={styles.home_button}
+                    variant="outline-primary"
+                >
+                    <i className="fa-solid fa-home" />
+                </Button>
             </div>
         </div>
     );
