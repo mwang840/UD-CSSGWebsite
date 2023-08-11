ec2fce5ff6553c979738ad90b2fa97ac6792438c
cthacker-udel <cthacker@udel.edu>
2023-08-10 23:55:01 -0400

    [landing_page] Added minimalist login page

--- src/modules/Login/Login.tsx
+++ src/modules/Login/Login.tsx
@@ -1,9 +1,17 @@
 import React from "react";
 
+import styles from "./Login.module.css";
+import { LoginForm } from "./LoginForm";
+
 /**
  *
  * @returns
  */
-const Login = (): JSX.Element => <div>{"Login"}</div>;
+const Login = (): JSX.Element => (
+    <div className={styles.login_page_container}>
+        <div className={styles.login_page_header}>{"Login to CS+SG"}</div>
+        <LoginForm />
+    </div>
+);
 
 export default Login;
