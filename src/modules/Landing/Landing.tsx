fd4d5d823c8c5bc91b3628f775dda6209a783913
cthacker-udel <cthacker@udel.edu>
2023-08-10 23:03:58 -0400

    [landing_page] Refactored landing page

--- src/modules/Landing/Landing.tsx
+++ src/modules/Landing/Landing.tsx
@@ -1,13 +1,15 @@
 "use client";
+import Image from "next/image";
 import React from "react";
 import { Button, OverlayTrigger } from "react-bootstrap";
 import type { OverlayInjectedProps } from "react-bootstrap/esm/Overlay";
 
 import _cssg from "@/assets/cs+sg.jpg";
-import { generatePopover } from "@/common/helper/popover/generatePopover";
+import CSSGLogo from "@/assets/cssg1.png";
+import { useBackgroundLinearGradientAnimation } from "@/hooks/color";
 
 import styles from "./Landing.module.css";
-import { useBackgroundLinearGradientAnimation } from "@/hooks/color";
+import { generateTooltip } from "@/common/helper/tooltip";
 
 /**
  *
@@ -18,31 +20,38 @@ const Landing = (): JSX.Element => {
     return (
         <div className={styles.landing_container}>
             <div className={styles.landing_header}>
+                <Image
+                    alt="CS+SG Clug Logo"
+                    className={styles.landing_club_logo}
+                    height={500}
+                    src={CSSGLogo.src}
+                    width={500}
+                />
                 {"Welcome to UD CS + Social Good!"}
             </div>
             <div className={styles.options_bar}>
                 <OverlayTrigger
                     overlay={(properties: OverlayInjectedProps): JSX.Element =>
-                        generatePopover(properties, "Login")
+                        generateTooltip(properties, "Login")
                     }
                     placement="bottom"
                 >
                     <Button
                         className={styles.landing_page_button}
-                        variant="outline-primary"
+                        variant="primary"
                     >
                         <i className="fa-solid fa-door-open" />
                     </Button>
                 </OverlayTrigger>
                 <OverlayTrigger
                     overlay={(properties: OverlayInjectedProps): JSX.Element =>
-                        generatePopover(properties, "Sign Up")
+                        generateTooltip(properties, "Sign Up")
                     }
                     placement="bottom"
                 >
                     <Button
                         className={styles.landing_page_button}
-                        variant="outline-secondary"
+                        variant="secondary"
                     >
                         <i className="fa-solid fa-user-plus" />
                     </Button>
