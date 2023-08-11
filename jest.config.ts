fc4d7c36ca8c697d89ae27b44b909aea91d0486f
cthacker-udel <cthacker@udel.edu>
2023-08-11 13:15:02 -0400

    [landing_page] Added fix to jest.config.ts file

--- jest.config.ts
+++ jest.config.ts
@@ -1,11 +1,12 @@
-import nextJest from "next/jest";
+/* eslint-disable node/no-unpublished-import -- disabled */
 import type { Config } from "jest";
+import nextJest from "next/jest";
 
 const createJestConfig = nextJest({ dir: "./" });
 
 const customJestConfig: Config = {
-  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
-  testEnvironment: "jest-environment-jsdom",
+    setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
+    testEnvironment: "jest-environment-jsdom",
 };
 
 export default createJestConfig(customJestConfig);
