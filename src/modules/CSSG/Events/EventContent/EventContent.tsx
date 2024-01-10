a03919b3ed241fed83a79955145fd85862e8c8bd
Maxwell Wang <maxwang@udel.edu>
2024-01-09 19:48:18 -0500

    all pages complete

--- src/modules/CSSG/Events/EventContent/EventContent.tsx
+++ src/modules/CSSG/Events/EventContent/EventContent.tsx
@@ -4,17 +4,17 @@ import React from "react";
 import { useCSSInjection } from "@/hooks/injection";
 import { CssgFormatMessage } from "@/lib/react-intl/CssgFormatMessage";
 
-import styles from "./FAQContent.module.css";
+import styles from "./EventContent.module.css";
 
 /**
  *
  * @returns the content page for the frequently asked questions page!
  */
-export const FAQContent = (): JSX.Element => {
-    useCSSInjection([{ className: styles.FAQ_body, cssQuery: "body" }]);
+export const EventContent = (): JSX.Element => {
+    useCSSInjection([{ className: styles.Events_body, cssQuery: "body" }]);
     return (
         <h1 className={styles.title}>
-            <CssgFormatMessage id="faq" />
+            <CssgFormatMessage id="events" />
         </h1>
     );
 };
